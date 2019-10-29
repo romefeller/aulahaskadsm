cbcee4311cc173922ff07144471af7669876f5e0
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-10-29 18:35:54 +0000

    DEPLOY

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ ssh root@51.91.126.117 <<EOF
 cd aulahaskadsm &&
 git pull origin master &&
 stack build &&
-lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9 &&
+lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
 nohup stack exec aulahaskell 
 EOF
