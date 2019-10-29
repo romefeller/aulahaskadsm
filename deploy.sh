09cca28c81b9f5b4aa003734683d77a22ec00347
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-10-29 18:16:37 +0000

    DEPLOY

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ git add .
 git commit -m $1
 git push origin master
 ssh root@51.91.126.117 <<EOF
-cd aulahaskadsm & git pull origin master & stack build
+cd aulahaskadsm 
+git pull origin master
+stack build
 EOF
