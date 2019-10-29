1dedd4df57f9f908b0c971081aec3b43449ce709
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-10-29 18:27:51 +0000

    DEPLOY

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@ git push origin master
 ssh root@51.91.126.117 <<EOF
 cd aulahaskadsm &&
 git pull origin master &&
-stack build &&
-lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9 &&
-nohup stack exec aulahaskell &
+stack build 
 EOF
