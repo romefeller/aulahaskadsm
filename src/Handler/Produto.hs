9770ad87688f5b7ff15b42c9083878a8ac2d4309
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-11-06 00:17:28 +0000

    LISTA DE PRODUTO

--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -49,3 +49,15 @@ postProdutoR = do
             |]
             redirect ProdutoR
         _ -> redirect HomeR
+
+getListProdR :: Handler Html 
+getListProdR = do 
+    -- select * from Produto order by produto.nome
+    produtos <- runDB $ selectList [] [Asc ProdutoNome]
+    $(whamletFile "templates/produtos.hamlet")
+
+postApagarProdR :: ProdutoId -> Handler Html
+postApagarProdR pid = do 
+    _ <- runDB $ get404 pid
+    runDB $ delete pid
+    redirect ListProdR
